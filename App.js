85ac14b9a4a90ede80ea9a28687a0537ffe15823
taouraghti <anasidrissi951@gmail.com>
2021-02-20 13:53:44 +0100

    fix-admob

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import CarsList from './components/CarsList'
 import Header from './components/Header'
+
 export default function App() {
   return (
     <View style={styles.container}>
--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {View, Text, ImageBackground} from 'react-native'
 import styles from './styles'
 import StyledButton from '../StyledButton'
+import {AdMobBanner,
+    AdMobInterstitial,
+    PublisherBanner,
+    AdMobRewarded,
+    setTestDeviceIDAsync} from 'expo-ads-admob'
+
  function CarItem(props) {
 
+        useEffect(() => {
+            async function loadAd(){
+                await AdMobInterstitial.setAdUnitID('ca-app-pub-3940256099942544/1033173712');
+                interstitialAd();
+
+            }
+            loadAd();
+        }, []);
+
+        async function interstitialAd(){
+            await AdMobInterstitial.requestAdAsync({ servePersonalizedAds: true});
+            await AdMobInterstitial.showAdAsync();
+        }
+      
     const {name, tagline, taglineCTA, image} = props.car;
     return (
             <View style={styles.carContainer}>
@@ -29,12 +49,17 @@ import StyledButton from '../StyledButton'
                         console.warn("costum order was pressed"); 
                         }}
                     />
+                    
                     <StyledButton 
                         type="secondary"
                         content="Existing Inventory"
                         onPress={() => {
                         console.warn("existing was pressed"); 
                         }}
+                    />
+                    <AdMobBanner
+                        bannerSize="fullBanner"
+                        adUnitID="ca-app-pub-3940256099942544/6300978111"
                     />
                 </View>
             </View>
